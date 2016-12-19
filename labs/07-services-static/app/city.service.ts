48e547f98b137f6e22e46195fd90c23fb009a772
Muhammad Hewedy <mhewedy@gmail.com>
2016-12-19 11:49:09 +0300

    type the service myself

--- labs/07-services-static/app/city.service.ts
+++ labs/07-services-static/app/city.service.ts
@@ -1,25 +1,19 @@
-import {Injectable} from '@angular/core';
-import  {City} from './city.model'
+import {Injectable} from '@angular/core'
+import {City} from './city.model'
 
 // @Injectable()
-export class CityService {
-	private cities: City[] = [
-		new City(1, 'Groningen', 'Groningen'),
-		new City(2, 'Hengelo', 'Overijssel'),
-		new City(3, 'Den Haag', 'Zuid-Holland'),
-		new City(4, 'Enschede', 'Overijssel')
-	];
+export class CityService{
 
-	// retourneer alle cities
-	getCities(): City[] {
-		return this.cities
-	}
+    private cities: City[] = [
+        new City(1, 'Cairo', 'Cairo'),
+        new City(2, 'Alex', 'Alex')
+    ];
 
-	// retourneer city op basis van ID
-	getCity(id: number) {
-		return this.cities.find(c => c.id === id);
-		// andere optie: .filter() gebruiken. Als je
-		// *meerdere* resultaten zou kunnen verwachten en dus een array wilt retourneren
-		// return this.cities.filter(c => c.id === id)[0]; // In dit geval: index [0] omdat we maar 1 city willen
-	}
+    getCities(): Array<City>{
+         return this.cities;
+    }
+
+    getCity(id: number): City {
+        return this.cities.find(c => c.id == id);
+    }
 }
